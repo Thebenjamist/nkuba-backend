2076f132039b6944b5a1b37ce2c7c9c7ee858eaa
Benjamin Ho <benho061995@gmail.com>
2022-09-14 12:50:09 +0200

    Created generic dynamo functions

---
+++ lambdas/common/dynamo.js
@@ -0,0 +1,103 @@
+const AWS = require("aws-sdk");
+
+let options = {};
+
+// if (process.env.IS_OFFLINE) {
+options = {
+  region: "localhost",
+  endpoint: "http://localhost:8000",
+};
+// }
+
+const documentClient = new AWS.DynamoDB.DocumentClient(options);
+
+const Dynamo = {
+  async get(id, TableName) {
+    const params = {
+      TableName,
+      Key: {
+        id,
+      },
+    };
+
+    const data = await documentClient.get(params).promise();
+
+    if (!data || !data.Item) {
+      throw Error(
+        `There was an error fetching the data of id of ${id} from ${TableName}`
+      );
+    }
+
+    return data.Item;
+  },
+
+  async write(data, TableName) {
+    if (!data.id) {
+      throw Error("No id in the data");
+    }
+    const params = {
+      TableName,
+      Item: data,
+    };
+
+    await documentClient.put(params).promise();
+  },
+
+  async scan({ FilterExpression, ExpressionAttributeValues, TableName }) {
+    const params = {
+      TableName,
+      FilterExpression,
+      ExpressionAttributeValues,
+    };
+
+    const data = await documentClient.scan(params).promise();
+
+    if (!data || !data.Items) {
+      throw Error(`There was an error fetching the data from ${TableName}`);
+    }
+
+    return data.Items;
+  },
+
+  async delete(id, TableName) {
+    if (!id) {
+      throw Error("No id in the data");
+    }
+    const params = {
+      TableName,
+      Key: {
+        id,
+      },
+    };
+
+    await documentClient.delete(params).promise();
+
+    return;
+  },
+
+  async update({
+    Key,
+    UpdateExpression,
+    ConditionExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
+  }) {
+    if (!data.id) {
+      throw Error("No id in the data");
+    }
+    const params = {
+      TableName,
+      Key,
+      UpdateExpression,
+      ConditionExpression,
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+    };
+
+    await documentClient.update(params).promise();
+
+    return;
+  },
+};
+
+module.exports = Dynamo;
